044674094fa1aeb0f458f6a0c3d2c8f3370ca2aa
SkeletalVirus <61529324+SkeletalVirus@users.noreply.github.com>
2023-12-11 10:54:13 -0500

    Add files via upload

---
+++ js/downloadContent.js
@@ -0,0 +1,11 @@
+const minecraft = {
+	name: "Minecraft",
+	imageFile: "minecraft.png",
+	requirements: "Minecraft Account",
+	downloadWindows: "https://drive.google.com/file/d/1G4lay1rc_0r4Ppk_YOlLw_ENtQ-WChfG/view?usp=drive_link",
+	downloadMac: "https://drive.google.com/file/d/1pxs6tislljDlZQw9qbDz_Zh8Hw3L2qkz/view?usp=drive_link",
+	downloadLinux: "https://drive.google.com/file/d/1aLRTc-ADKf5T8KAZ2wsDAW53RyZTfh60/view?usp=drive_link"
+	};
+	
+
+	
